7f95a541b13035f5eee19a75078d5788b89c996f
Carrie Kraft <carriekarft@gmail.com>
2023-06-23 10:32:31 -0700

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    # { message: "Good luck with your project!" }.to_json
+    '<h1>Hello!</h1>'
   end
 
 end
