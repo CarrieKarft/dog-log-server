3b6d5dcf531927e0269b77b1b4ef24ade4e039ed
Carrie Kraft <carriekarft@gmail.com>
2023-06-25 06:20:05 -0700

    adding get method to retrive all the data in the dogs table, and sending back a response as JSON

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    # { message: "Good luck with your project!" }.to_json
-    '<h1>Hello!</h1>'
+#   # Add your routes here
+  get "/dogs" do
+    dogs = Dog.all
+    dogs.to_json
   end
+
+#   get '/dogs/:id/walks' do 
+#     dog = Dog.find(params[:id])
+#     walks = dog.walks
+#     walks.to_json
+#   end
 
 end
