5da1cffbb30a13a708aa806a1b4a35389b95ae5b
Carrie Kraft <carriekarft@gmail.com>
2023-06-27 19:09:09 -0700

    adding post methods to create new instances of both the Wlak and Dog class, and save them to the db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,28 @@ class ApplicationController < Sinatra::Base
     dog.to_json(include: :walks)
   end
 
+  post '/walks' do
+    walk = Walk.create(
+      location: params[:location],
+      used_bathroom: params[:used_bathroom],
+      duration_in_minutes: params[:duration_in_minutes],
+      distance_in_miles: params[:distance_in_miles],
+      medication_given: params[:medication_given],
+      notes: params[:notes],
+      dog_id: params[:dog_id]
+    )
+    walk.to_json
+  end
+
+  post '/dogs' do 
+    dog = Dog.create(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age],
+      energy_level: params[:energy_level],
+      medication: params[:medication]
+    )
+    dog.to_json
+  end
+
 end
