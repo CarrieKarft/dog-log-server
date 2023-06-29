b35c3a506147bc17adbb81068ab04df757adaa43
Carrie Kraft <carriekarft@gmail.com>
2023-06-29 06:21:46 -0700

    adding patch method for '/walks/:id' to update the values for a specified walk

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,5 +44,19 @@ class ApplicationController < Sinatra::Base
     walk.to_json
   end
 
+  patch '/walks/:id' do 
+    walk = Walk.find(params[:id])
+    walk.update(
+      location: params[:location],
+      used_bathroom: params[:used_bathroom],
+      duration_in_minutes: params[:duration_in_minutes],
+      distance_in_miles: params[:distance_in_miles],
+      medication_given: params[:medication_given],
+      notes: params[:notes]
+    )
+    walk.to_json
+  end
+
+
 
 end
